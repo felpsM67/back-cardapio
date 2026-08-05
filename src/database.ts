4448d2858b9f809c590edc221cd82fb58ae1da6d
felipe <felipedasilvamattos50@gmail.com>
2026-08-05 16:58:50 -0400

    atualizações

--- src/database.ts
+++ src/database.ts
@@ -1,15 +1,43 @@
-import { Dialect, Sequelize } from "sequelize";
+import { Sequelize } from "sequelize";
+
 import { ENV } from "@/config/env";
 
 const isTest = ENV.NODE_ENV === "test";
+const databaseUrl = process.env.DATABASE_URL;
+
+if (!isTest && !databaseUrl) {
+  throw new Error(
+    "A variável DATABASE_URL não foi configurada no .env.",
+  );
+}
 
 const sequelize = isTest
-  ? new Sequelize({ dialect: "sqlite", storage: ":memory:", logging: false })
-  : new Sequelize(ENV.DB_DATABASE, ENV.DB_USERNAME, ENV.DB_PASSWORD, {
-      host: ENV.DB_HOST,
-      port: ENV.DB_PORT,
-      dialect: ENV.DB_DIALECT as Dialect,
-      logging: ENV.NODE_ENV === "development" ? console.log : false,
+  ? new Sequelize({
+      dialect: "sqlite",
+      storage: ":memory:",
+      logging: false,
+    })
+  : new Sequelize(databaseUrl as string, {
+      dialect: "postgres",
+
+      logging:
+        ENV.NODE_ENV === "development"
+          ? console.log
+          : false,
+
+      dialectOptions: {
+        ssl: {
+          require: true,
+          rejectUnauthorized: false,
+        },
+      },
+
+      pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000,
+      },
     });
 
 export default sequelize;
